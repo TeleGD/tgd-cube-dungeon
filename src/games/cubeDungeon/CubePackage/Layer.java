48556a77504718e7b9e75574352fda49daecc8b8
joannes-guichon <55346667+joannes-guichon@users.noreply.github.com>
2019-11-21 17:59:21 +0100

    Update Layer.java
    Affichage Layer

--- src/games/cubeDungeon/CubePackage/Layer.java
+++ src/games/cubeDungeon/CubePackage/Layer.java
@@ -2,18 +2,28 @@ package games.cubeDungeon.CubePackage;
 
 import java.util.ArrayList;
 
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.state.StateBasedGame;
+
 public class Layer {
 
 	private ArrayList<Face> faces;
+	private Face top, bottom, front, left, right, back;
 	
 	public Layer(int face_width) {
-		
-		faces = new ArrayList<Face>();
-		for (int i = 0; i < 6; i++)
-			faces.add(new Face(face_width));
+		this.front = new Face(face_width);
+		this.left = new Face(face_width);
+		this.right = new Face(face_width);
+		this.back = new Face(face_width);
+		this.top = new Face(face_width);
+		this.bottom = new Face(face_width);
 		
 	}
 	
+	public void render(GameContainer container, StateBasedGame game, Graphics context) {
+		front.render(container, game, context);
+	}
 	
 	
 	
