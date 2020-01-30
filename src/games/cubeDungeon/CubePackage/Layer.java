4b5882c101394ac31d2ef4733f86cb617b061155
Clément Crouzet <clement.crouzet@telecomnancy.eu>
2020-01-30 15:59:44 +0100

    current_face

--- src/games/cubeDungeon/CubePackage/Layer.java
+++ src/games/cubeDungeon/CubePackage/Layer.java
@@ -10,19 +10,20 @@ public class Layer {
 
 	private ArrayList<Face> faces;
 	private Face top, bottom, front, left, right, back;
-
+	private Face current_face;
 	
 	public Layer(int face_width, int screen_width, int screen_height, int N) {
-		this.front = new Face(face_width, screen_width, screen_height, N);
-		this.left = new Face(face_width, screen_width, screen_height, N);
-		this.right = new Face(face_width, screen_width, screen_height, N);
-		this.back = new Face(face_width, screen_width, screen_height, N);
-		this.top = new Face(face_width, screen_width, screen_height, N);
+		this.front  = new Face(face_width, screen_width, screen_height, N);
+		this.left   = new Face(face_width, screen_width, screen_height, N);
+		this.right  = new Face(face_width, screen_width, screen_height, N);
+		this.back   = new Face(face_width, screen_width, screen_height, N);
+		this.top    = new Face(face_width, screen_width, screen_height, N);
 		this.bottom = new Face(face_width, screen_width, screen_height, N);
+		this.current_face = this.front;
 	}
 	
 	public void render(GameContainer container, StateBasedGame game, Graphics context) {
-		front.render(container, game, context);
+		current_face.render(container, game, context);
 	}
 	
 	
