7db9f2112b5510b67368b48e6f8686cb238e6d9a
Paul Chipot <paul.chipot@telecomnancy.eu>
2020-03-12 13:50:26 +0100

    petite modif

--- src/games/cubeDungeon/CubePackage/Face.java
+++ src/games/cubeDungeon/CubePackage/Face.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.state.StateBasedGame;
+import java.util.Random;
+//import java.lang.Math.*;
 
 public class Face {
 
@@ -12,7 +14,7 @@ public class Face {
 	private int face_width;
 
 
-	
+
 	public Face(int face_width, int screen_width, int screen_height, int N) {
 		this.face_width = face_width;
 		grille = new Tile[this.face_width][this.face_width];
@@ -21,8 +23,24 @@ public class Face {
 				grille[i][j] = new Tile(i,j, screen_width, screen_height, this.face_width, N);
 			}
 		}
+		this.generation();
 	}
-	
+
+	private void generation() {
+		Random rand = new Random();
+		for (int k = 0 ; k < this.face_width ; k++) {
+			int x = rand.nextInt(this.face_width);
+			int y = rand.nextInt(this.face_width);
+			grille[x][y].changeType(Tile.TypeSol.roche);
+		}
+	}
+	//double choix = Math.random();
+	//double broke = Math.random();
+
+
+
+
+
 	public void render(GameContainer container, StateBasedGame game, Graphics context) {
 		for (int i = 0; i < this.face_width; i++) {
 			for (int j = 0; j < this.face_width; j++) {
@@ -30,11 +48,11 @@ public class Face {
 			}
 		}
 	}
-	
+
 	public Tile getTile(int i, int j) {
 		return(this.grille[i][j]);
 	}
-	
+
 	public int getSize() {
 		return face_width;
 	}
